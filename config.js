47aad5ba7450b47751995d97237e0dcfb885aa3b
Nur Kholis Majid <nurkholismajid77@gmail.com>
2026-07-11 23:31:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
    Isi sekali, aman selamanya.
    ============================================================ */
 window.PP_CONFIG = {
-  url: "https://rgqnbuplwcxrkjvhgmkf.supabase.co",   // <- sudah sesuai project-mu
-  key: "PASTE-ANON-OR-PUBLISHABLE-KEY-DI-SINI",      // <- tinggal isi ini
+  url: "https://rgqnbuplwcxrkjvhgmkf.supabase.co",   // <- sudah sesuai project-mu 
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJncW5idXBsd2N4cmtqdmhnbWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3MjA1ODAsImV4cCI6MjA5OTI5NjU4MH0.DIQclBZWbRqY5-Suas8HXcOtT0iV_ULrAaI-e0pCsG8",      // <- tinggal isi ini
 };
